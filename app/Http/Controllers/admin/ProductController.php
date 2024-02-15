0292643892059e7d62b1913389084baf301125a4
richa <aliado.richa@gmail.com>
2024-02-15 10:21:42 +0800

    product crud

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -7,11 +7,13 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductImage;
+use App\Models\SubCategory;
 use App\Models\TempImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -126,5 +128,116 @@ class ProductController extends Controller
                 'errors' => $validator->errors()
             ]);
         }
+    }
+
+    public function edit($id, Request $request) {
+
+        $product = Product::find($id);
+
+        if (empty($product)) {
+            return redirect()->route('products.index')->with('error', 'Product not found');
+        }
+        
+        // fetch product imgaes
+        $productImages = ProductImage::where('product_id',$product->id)->get();
+
+        $subCategories = SubCategory::where('category_id',$product->category_id)->get();
+
+        $data = [];    
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $brands = Brand::orderBy('name', 'ASC')->get();
+        $data['categories'] = $categories;
+        $data['brands'] = $brands;
+        $data['product'] = $product;
+        $data['subCategories'] = $subCategories;
+        $data['productImages'] = $productImages;
+        return view('admin.products.edit',$data);
+    }
+
+    public function update($id, Request $request) {
+        $product = Product::find($id);
+
+        $rules = [
+            'title' => 'required',
+            'slug' => 'required|unique:products,slug,'.$product->id.',id',
+            'price' => 'required|numeric',
+            'sku' => 'required|unique:products,slug,'.$product->id.',id',
+            'track_qty' => 'required|in:Yes,No',
+            'category' => 'required|numeric',
+            'is_featured' => 'required|in:Yes,No',
+        ];
+    
+        if (!empty($request->track_qty) && $request->track_qty == 'Yes'){
+            $rules['qty'] = 'required|numeric';
+        }
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->passes()) {
+
+            $product->title = $request->title;
+            $product->slug = $request->slug;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->compare_price = $request->compare_price;
+            $product->sku = $request->sku;
+            $product->barcode = $request->barcode;
+            $product->track_qty = $request->track_qty;
+            $product->qty = $request->qty;
+            $product->status = $request->status;
+            $product->category_id = $request->category;
+            $product->sub_category_id = $request->sub_category;
+            $product->brand_id = $request->brand;
+            $product->is_featured = $request->is_featured;
+            $product->save();
+
+
+            //save gallery images
+            
+            session()->flash('success','Product updated successfully.');
+            return response()->json([
+                'status' => true,
+                'message' => 'Product updated successfully.'
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+
+    public function destroy($id, Request $request) {
+        $product = Product::find($id);
+
+        if (empty($product)) {
+            session()->flash('error', 'Product not found');
+            return response()->json([
+                'status' => false,
+                'notFound' => true
+            ]);
+        }
+
+        $productImages = ProductImage::where('product_id',$id)->get();
+
+        if (!empty($productImages)) {
+            foreach ($productImages as $productImage) {
+                File::delete(public_path('uploads/product/large/'.$productImage->image));
+                File::delete(public_path('uploads/product/small/'.$productImage->image));
+            }
+
+            $productImages = ProductImage::where('product_id',$id)->delete();
+
+        }
+
+        $product->delete();
+
+        session()->flash('success', 'Product deleted successfully');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product deleted successfully'
+        ]);
     }
 }
